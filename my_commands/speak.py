ab58e1cb99c0fd22fb981324911a6e554ae0d363
brent <br3ntor@gmail.com>
2023-07-16 02:09:28 -0700

    Reverted speak to its job

--- my_commands/speak.py
+++ my_commands/speak.py
@@ -1,17 +1,8 @@
 import discord
 from discord import app_commands
 
-group = app_commands.Group(name="uwu", description="Lets try this!")
 
-
-# @app_commands.command()
-@group.command()
+@app_commands.command()
 async def speak(interaction: discord.Interaction, message: str):
     """Make the bot speak."""
     await interaction.response.send_message(message)
-
-
-@group.command()
-async def say_poop(interaction: discord.Interaction):
-    """A very important message."""
-    await interaction.response.send_message("Poop")
