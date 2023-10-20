a211d0197259aa30d7eca4e3969a4e7e669ff283
br3ntor <br3ntor@gmail.com>
2023-10-20 01:28:32 -0700

    Starting refactor for single server

--- my_commands/__init__.py
+++ my_commands/__init__.py
@@ -1,27 +1,27 @@
 from .cat_fact import cat_fact
-from .speak import speak
-from .send_message import send_message
-from .restart_server import restart_server
-from .get_playerlist import get_playerlist
-from .reset_password import reset_password
-from .update_mods_list import update_mods_list
-from .restart_server_auto import restart_server_auto
-from .restart_server_auto import cancel_auto_restart
-from .admin import admin_group
-from .update_sandbox_gist import update_sandbox_gist
-from .ban import ban_group
+#from .speak import speak
+#from .send_message import send_message
+#from .restart_server import restart_server
+#from .get_playerlist import get_playerlist
+#from .reset_password import reset_password
+#from .update_mods_list import update_mods_list
+#from .restart_server_auto import restart_server_auto
+#from .restart_server_auto import cancel_auto_restart
+#from .admin import admin_group
+#from .update_sandbox_gist import update_sandbox_gist
+#from .ban import ban_group
 
 __all__ = [
     "cat_fact",
-    "speak",
-    "send_message",
-    "restart_server",
-    "get_playerlist",
-    "reset_password",
-    "update_mods_list",
-    "restart_server_auto",
-    "cancel_auto_restart",
-    "admin_group",
-    "update_sandbox_gist",
-    "ban_group",
+#    "speak",
+#    "send_message",
+#    "restart_server",
+#    "get_playerlist",
+#    "reset_password",
+#    "update_mods_list",
+#    "restart_server_auto",
+#    "cancel_auto_restart",
+#    "admin_group",
+#    "update_sandbox_gist",
+#    "ban_group",
 ]
