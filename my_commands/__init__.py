771f0277310079e1fbbd654867b7cc5f6b64f364
br3ntor <br3ntor@gmail.com>
2023-10-25 00:46:33 -0700

    Enabled restart command

--- my_commands/__init__.py
+++ my_commands/__init__.py
@@ -1,7 +1,7 @@
 from .cat_fact import cat_fact
 #from .speak import speak
 from .send_message import send_message
-#from .restart_server import restart_server
+from .restart_server import restart_server
 #from .get_playerlist import get_playerlist
 #from .reset_password import reset_password
 #from .update_mods_list import update_mods_list
@@ -15,7 +15,7 @@ __all__ = [
     "cat_fact",
 #    "speak",
    "send_message",
-#    "restart_server",
+   "restart_server",
 #    "get_playerlist",
 #    "reset_password",
 #    "update_mods_list",
