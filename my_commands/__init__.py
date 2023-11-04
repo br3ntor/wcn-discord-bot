e12fe43e19f6edfe4a4affbf8d0ab7edf46077e6
br3ntor <br3ntor@gmail.com>
2023-11-03 19:30:36 -0700

    Refactored get_playerlist for single server

--- my_commands/__init__.py
+++ my_commands/__init__.py
@@ -2,7 +2,7 @@ from .cat_fact import cat_fact
 #from .speak import speak
 from .send_message import send_message
 from .restart_server import restart_server
-#from .get_playerlist import get_playerlist
+from .get_playerlist import get_playerlist
 #from .reset_password import reset_password
 from .update_mods_list import update_mods_list
 #from .restart_server_auto import restart_server_auto
@@ -16,7 +16,7 @@ __all__ = [
 #    "speak",
    "send_message",
    "restart_server",
-#    "get_playerlist",
+   "get_playerlist",
 #    "reset_password",
    "update_mods_list",
 #    "restart_server_auto",
--- my_commands/get_playerlist.py
+++ my_commands/get_playerlist.py
@@ -5,8 +5,7 @@ from steam import game_servers as gs
 from tabulate import tabulate
 import time
 
-VANILLA_SERVER = os.getenv("VANILLA_SERVER")
-MODDED_SERVERS = os.getenv("MODDED_SERVERS")
+SERVER_IP = os.getenv("SERVER_IP")
 
 
 def format_time(seconds):
@@ -14,9 +13,9 @@ def format_time(seconds):
     return time_str
 
 
-def format_message(player_table, server_name):
+def format_message(player_table):
     msg = f"""```md
-I can see {len(player_table)} players on the {server_name} server.
+I can see {len(player_table)} players on the server.
 
 {tabulate(player_table, headers=["Name", "Duration"])}
     ```"""
@@ -24,30 +23,15 @@ I can see {len(player_table)} players on the {server_name} server.
 
 
 @app_commands.command()
-@app_commands.choices(
-    server=[
-        app_commands.Choice(name="Vanilla", value=1),
-        app_commands.Choice(name="Light", value=2),
-        app_commands.Choice(name="Heavy", value=3),
-    ]
-)
-async def get_playerlist(
-    interaction: discord.Interaction, server: app_commands.Choice[int]
-):
+async def get_playerlist(interaction: discord.Interaction):
     """Get a list of players on the server."""
-    servers = {
-        "vanilla": (VANILLA_SERVER, 16261),
-        "light": (MODDED_SERVERS, 16261),
-        "heavy": (MODDED_SERVERS, 27901),
-    }
-
-    server_players = gs.a2s_players(servers[server.name.lower()])
+    server_players = gs.a2s_players((SERVER_IP, 16261))
 
     player_table = []
     for player in server_players:
         if player["name"]:
             player_table.append([player["name"], format_time(player["duration"])])
 
-    formated_message = format_message(player_table, server.name)
+    formated_message = format_message(player_table)
 
     await interaction.response.send_message(formated_message)
