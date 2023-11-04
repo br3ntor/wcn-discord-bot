dc698a7767471b253105089f116ab0b08c0ee984
br3ntor <br3ntor@gmail.com>
2023-11-04 02:47:56 -0700

    Auto restart single server refactor

--- my_commands/__init__.py
+++ my_commands/__init__.py
@@ -5,8 +5,8 @@ from .restart_server import restart_server
 from .get_playerlist import get_playerlist
 from .reset_password import reset_password
 from .update_mods_list import update_mods_list
-#from .restart_server_auto import restart_server_auto
-#from .restart_server_auto import cancel_auto_restart
+from .restart_server_auto import restart_server_auto
+from .restart_server_auto import cancel_auto_restart
 from .admin import admin_group
 from .update_sandbox_gist import update_sandbox_gist
 #from .ban import ban_group
@@ -19,8 +19,8 @@ __all__ = [
    "get_playerlist",
    "reset_password",
    "update_mods_list",
-#    "restart_server_auto",
-#    "cancel_auto_restart",
+   "restart_server_auto",
+   "cancel_auto_restart",
    "admin_group",
    "update_sandbox_gist",
 #    "ban_group",
--- my_commands/restart_server_auto.py
+++ my_commands/restart_server_auto.py
@@ -10,20 +10,20 @@ ANNOUNCE_CHANNEL = int(os.getenv("ANNOUNCE_CHANNEL"))
 COUNT_DOWN_TIME = 300  # seconds
 
 # Track if countdown timer is running
-countdown_isrunning = {"light": False, "heavy": False}
+countdown_isrunning = False
 
 # Will abort any restart function if a count_down_started is True
 abort_signal = False
 
 
 # TODO: Add try catch for proper error handeling, maybe return true or success message
-async def send_server_msg(server, message):
-    server_msg = "'servermsg \"" + message + "\"'"
+async def send_server_msg(message):
+    server_msg = f'servermsg "{message}"'
     cmd = [
         "runuser",
-        f"pzserver{server}",
+        "pzserver",
         "-c",
-        f"/home/pzserver{server}/pzserver send {server_msg}",
+        f"/home/pzserver/pzserver send '{server_msg}'",
     ]
     process = await asyncio.create_subprocess_exec(
         *cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE
@@ -34,72 +34,29 @@ async def send_server_msg(server, message):
     print(error.decode())
 
 
-class RestartServerView(discord.ui.View):
+class Confirm(discord.ui.View):
     def __init__(self):
         super().__init__()
-        self.server = None
         self.value = None
 
-    @discord.ui.select(
-        cls=discord.ui.Select,
-        placeholder="Choose server...",
-        min_values=1,
-        max_values=1,
-        options=[
-            discord.SelectOption(
-                label="Light", description="The noob server", emoji="🥗"
-            ),
-            discord.SelectOption(
-                label="Heavy", description="The pro server", emoji="🍖"
-            ),
-        ],
-    )
-    async def select_channels(
-        self, interaction: discord.Interaction, select: discord.ui.Select
-    ):
-        # Enable buttons after channel select
-        for element in self.children:
-            if isinstance(element, discord.ui.Button):
-                element.disabled = False
-            if isinstance(element, discord.ui.Select):
-                emoji = "🥗" if select.values[0].lower() == "light" else "🍖"
-                # TODO: I learned there is a default option in the
-                # discord.SelectOption method Another option might
-                # be to set that instead of placeholder?
-                element.placeholder = f"{emoji} {select.values[0]} Server"
-
-        self.server = select.values[0].lower()
-        await interaction.response.edit_message(view=self)
-
-    @discord.ui.button(
-        label="Yes", style=discord.ButtonStyle.green, disabled=True, emoji="🔌"
-    )
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def confirm(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
-        server = "unknown"  # hehehe
-        for element in self.children:
-            if isinstance(element, discord.ui.Select):
-                server = element.values[0]
-
-        emoji = "🥗" if server.lower() == "light" else "🍖"
-        msg = (
-            f"Restart initiated for the {emoji}**{server.upper()}** server by "
-            f"{interaction.user.display_name}, restarting in {COUNT_DOWN_TIME} seconds."
+        await interaction.response.send_message(
+            "Auto Restart Confirmed.", ephemeral=True
         )
-        await interaction.response.send_message(msg)
         self.value = True
         self.stop()
 
     # This one is similar to the confirmation button except
     # sets the inner value to `False`
-    @discord.ui.button(
-        label="No", style=discord.ButtonStyle.grey, disabled=False, emoji="🤡"
-    )
+    @discord.ui.button(label="No", style=discord.ButtonStyle.grey)
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(
-            f"{interaction.user.display_name} changed their mind."
-        )
+        await interaction.response.send_message("Auto Restart Aborted.", ephemeral=True)
         self.value = False
         self.stop()
 
@@ -114,10 +71,10 @@ async def restart_server_auto(
     abort_signal = False
 
     # Create the view containing our dropdown and buttons
-    view = RestartServerView()
+    view = Confirm()
 
     await interaction.response.send_message(
-        "So you want to restart the...", view=view, ephemeral=True
+        "Automatically restart the server after 5min?", view=view, ephemeral=True
     )
     await view.wait()
 
@@ -132,22 +89,19 @@ async def restart_server_auto(
         print("Confirmed...")
 
         # Announce to discord members restart will happen after some minutes
-        emoji = "🥗" if view.server == "light" else "🍖"
         init_msg = (
-            f"Restart initiated for the {emoji}**{view.server.upper()}** "
+            "Restart initiated for the Zomboid server."
             f"server, restarting in {COUNT_DOWN_TIME//60} minutes."
         )
 
         # Send players on server first restart warning
-        await send_server_msg(
-            view.server, f"Server will restart in {COUNT_DOWN_TIME//60} minutes."
-        )
+        await send_server_msg(f"Server will restart in {COUNT_DOWN_TIME//60} minutes.")
 
         # Send message to discord members announcing restart
         await interaction.guild.get_channel(ANNOUNCE_CHANNEL).send(init_msg)
 
         # Start tracking
-        countdown_isrunning[view.server] = True
+        countdown_isrunning = True
         start_time = asyncio.get_event_loop().time()
         end_time = start_time + COUNT_DOWN_TIME
         ran_once = False
@@ -156,12 +110,12 @@ async def restart_server_auto(
             if abort_signal:
                 # TODO: Consider adding server message to
                 # inform players of abort in game
-                countdown_isrunning[view.server] = False
+                countdown_isrunning = False
                 await interaction.channel.send(
-                    f"Auto restart ABORTED 👼 for the {emoji}**{view.server}** server."
+                    "Auto restart ABORTED 👼 for the zomboid server."
                 )
                 return await interaction.guild.get_channel(ANNOUNCE_CHANNEL).send(
-                    f"Auto restart ABORTED 👼 for the {emoji}**{view.server}** server."
+                    "Auto restart ABORTED 👼 for the zomboid server."
                 )
 
             seconds_left = ceil(
@@ -176,34 +130,21 @@ async def restart_server_auto(
                     view.server, f"The server will restart in {seconds_left} seconds!"
                 )
                 await interaction.channel.send(
-                    f"The {emoji}**{view.server.upper()}** "
-                    f"server will restart in {seconds_left} seconds."
+                    f"The zomboid server will restart in {seconds_left} seconds."
                 )
-                # await interaction.guild.get_channel(ANNOUNCE_CHANNEL).send(
-                #     f"The {emoji}**{view.server.upper()}** server will restart in "
-                #     f"{seconds_left} seconds."
-                # )
-
-            # Send command every n seconds if seconds are less than b
-            # if seconds_left % 5 == 0 and seconds_left <= 15:
-            # if seconds_left <= 20:
-            #     await interaction.channel.send(
-            #         f"The {emoji}**{view.server.upper()}** server will restart in "
-            #         f"{seconds_left} seconds."
-            #     )
 
             await asyncio.sleep(5)
 
         # Countdown is over so lets reset count_down trackers
-        countdown_isrunning[view.server] = False
+        countdown_isrunning = False
 
         try:
-            cmd = ["systemctl", "restart", f"pzserver{view.server}"]
+            cmd = ["systemctl", "restart", "pzserver"]
             process = await asyncio.create_subprocess_exec(*cmd)
             await process.wait()
 
             restart_msg = (
-                f"Success! The {emoji}**{view.server.upper()}** server "
+                "Success! The zomboid server "
                 "was restarted and is now loading back up."
             )
             await interaction.followup.send(restart_msg)
@@ -220,7 +161,7 @@ async def cancel_auto_restart(interaction: discord.Interaction):
     """ZOMG CANCEL B4 IT TOO LATE!!!."""
     global abort_signal
 
-    if countdown_isrunning["light"] or countdown_isrunning["heavy"]:
+    if countdown_isrunning:
         abort_signal = True
         await interaction.response.send_message("Abort signal sent!")
     else:
