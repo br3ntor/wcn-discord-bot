2fe234fabdac7833535d302062bc1d8effcc6286
br3ntor <br3ntor@gmail.com>
2024-03-22 21:53:57 -0700

    Nothing really

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
 import logging
-from dotenv import load_dotenv
+import os
 
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
--- my_commands/__init__.py
+++ my_commands/__init__.py
@@ -1,15 +1,14 @@
+from .admin import admin_group
+from .ban import ban_group
 from .cat_fact import cat_fact
-from .speak import speak
-from .send_message import send_message
-from .restart_server import restart_server
 from .get_playerlist import get_playerlist
 from .reset_password import reset_password
+from .restart_server import restart_server
+from .restart_server_auto import cancel_auto_restart, restart_server_auto
+from .send_message import send_message
+from .speak import speak
 from .update_mods_list import update_mods_list
-from .restart_server_auto import restart_server_auto
-from .restart_server_auto import cancel_auto_restart
-from .admin import admin_group
 from .update_sandbox_gist import update_sandbox_gist
-from .ban import ban_group
 
 __all__ = [
     "cat_fact",
