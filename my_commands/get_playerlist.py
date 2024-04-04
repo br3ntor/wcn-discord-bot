b97cc2f9f3773fa4be2660a0d3c59a5de3b649d0
br3ntor <br3ntor@gmail>
2024-04-04 02:59:00 -0700

    playerlist and modupdatelist

--- my_commands/get_playerlist.py
+++ my_commands/get_playerlist.py
@@ -1,9 +1,10 @@
 import os
+import time
+
 import discord
 from discord import app_commands
 from steam import game_servers as gs
 from tabulate import tabulate
-import time
 
 SERVER_IP = os.getenv("SERVER_IP")
 
@@ -13,9 +14,9 @@ def format_time(seconds: float) -> str:
     return time_str
 
 
-def format_message(player_table: list, server_name: str) -> str:
+def format_message(player_table: list) -> str:
     msg = f"""```md
-I can see {len(player_table)} players on the {server_name} server.
+I can see {len(player_table)} players on the server.
 
 {tabulate(player_table, headers=["Name", "Duration"])}
     ```"""
@@ -23,29 +24,16 @@ I can see {len(player_table)} players on the {server_name} server.
 
 
 @app_commands.command()
-@app_commands.choices(
-    server=[
-        app_commands.Choice(name="Vanilla", value=1),
-        app_commands.Choice(name="Modded", value=2),
-    ]
-)
-async def get_playerlist(
-    interaction: discord.Interaction, server: app_commands.Choice[int]
-):
+async def get_playerlist(interaction: discord.Interaction):
     """Get a list of players on the server."""
 
-    servers = {
-        "vanilla": (SERVER_IP, 27901),
-        "modded": (SERVER_IP, 16261),
-    }
-
-    server_players = gs.a2s_players(servers[server.name.lower()])
+    server_players = gs.a2s_players((SERVER_IP, 16261))
 
     player_table = []
     for player in server_players:
         if player["name"]:
             player_table.append([player["name"], format_time(player["duration"])])
 
-    formated_message = format_message(player_table, server.name)
+    formated_message = format_message(player_table)
 
     await interaction.response.send_message(formated_message)
